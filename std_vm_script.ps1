158cf81d2c0616d1b768d989df7016e62f0007fc
Pradhan Biliye <98209649+pbiliye91@users.noreply.github.com>
2023-07-18 00:56:44 +0530

    Update std_vm_script.ps1

--- std_vm_script.ps1
+++ std_vm_script.ps1
@@ -1,2 +1,2 @@
 Get-Disk | Where-Object { $_.PartitionStyle -eq 'raw' } | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Disk" -Confirm:$false
-Get-Partition | Where-Object { $_.DiskNumber -eq 2 } | Set-Partition -NewDriveLetter 
+Get-Partition | Where-Object { $_.DiskNumber -eq 2 } | Set-Partition -NewDriveLetter F
