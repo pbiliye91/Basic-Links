bdde96231a33d27b7a08024066ab18c4afca56b3
Pradhan Biliye <98209649+pbiliye91@users.noreply.github.com>
2023-08-29 22:15:21 +0530

    Update new.ps1

--- new.ps1
+++ new.ps1
@@ -2,11 +2,11 @@ $diskNumber1 = 2
 $diskNumber2 = 3
 $diskNumber3 = 4
 
-# Initialize and format first disk without assigning a drive letter
-Get-Disk -Number $diskNumber1 | Initialize-Disk -PartitionStyle GPT -PassThru | New-Partition -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "data" -Confirm:$false
+# Initialize and format first disk without drive letter
+Get-Disk -Number $diskNumber1 | Initialize-Disk -PartitionStyle GPT -PassThru | New-Partition -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "data" -AssignDriveLetter $false -Confirm:$false
 
-# Initialize and format second disk without assigning a drive letter
-Get-Disk -Number $diskNumber2 | Initialize-Disk -PartitionStyle GPT -PassThru | New-Partition -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "log" -Confirm:$false
+# Initialize and format second disk without drive letter
+Get-Disk -Number $diskNumber2 | Initialize-Disk -PartitionStyle GPT -PassThru | New-Partition -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "log" -AssignDriveLetter $false -Confirm:$false
 
-# Initialize and format third disk without assigning a drive letter
-Get-Disk -Number $diskNumber3 | Initialize-Disk -PartitionStyle GPT -PassThru | New-Partition -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "tempDb" -Confirm:$false
+# Initialize and format third disk without drive letter
+Get-Disk -Number $diskNumber3 | Initialize-Disk -PartitionStyle GPT -PassThru | New-Partition -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "tempDb" -AssignDriveLetter $false -Confirm:$false
